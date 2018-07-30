7cf68dc860e2ccf07a2b6f22e80bd34f2b099096
ebkn <ktennis.mqekr12@gmail.com>
2018-07-31 02:02:15 +0900

    feat: enable async/await

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env', 'react']
+            presets: ['es2017', 'react']
           },
         },
       },
